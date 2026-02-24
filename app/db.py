74b03cf7849cf241596943e6124cd43b25eddc53
Angelus127 <valerohuertas9@gmail.com>
2026-02-24 16:42:37 -0500

    solucion railway 1.0

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from app.config import Config
 
 def conectar():
     try:
-        return psycopg2.connect(Config.DATABASE_URL)
+        return psycopg2.connect(Config.DATABASE_URL, sslmode="require")
     except Exception as e:
         print("Error al conectar a la base de datos: ", e)
         return None 
